f7cdd9db7efbd95aa5b08fde75e1f6c3367d9778
hackeru <hackerudroids@gmail.com>
2017-07-17 21:29:00 +0300

    new change

--- app/src/main/java/israel/firebasepushnotifications/MainActivity.java
+++ app/src/main/java/israel/firebasepushnotifications/MainActivity.java
@@ -1,12 +1,14 @@
 package israel.firebasepushnotifications;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -60,7 +62,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initWithUser(){
-
+        SharedPreferences prefs = getSharedPreferences("userid", MODE_PRIVATE);
+        String token = prefs.getString("token", null);
+        if (token == null){
+            Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+            Log.d("hackeru",token);
+        }
     }
 
     @Override
--- app/src/main/java/israel/firebasepushnotifications/MyFirebaseMessagingService.java
+++ app/src/main/java/israel/firebasepushnotifications/MyFirebaseMessagingService.java
@@ -1,6 +1,13 @@
 package israel.firebasepushnotifications;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by hackeru on 17/07/2017.
@@ -8,4 +15,31 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d("hackeru", "onMessageReceived");
+    }
+
+
+    @Override
+    public void handleIntent(Intent intent) {
+       // super.handleIntent(intent);
+        Log.d("hackeru", "handelIntent");
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("Hello custom push");
+        builder.setSmallIcon(R.drawable.ic_notification);
+        builder.setContentText("The Content Text");
+        builder.setAutoCancel(true);
+
+
+        Intent contentIntent = new Intent(this, MainActivity.class);
+        contentIntent.putExtra("name", "Israel");
+
+        PendingIntent pi = PendingIntent.getActivities(this, 1, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = builder.build();
+    }
 }
